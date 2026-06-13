bc160600b5cf47cc7503ef61035e63cb38a5cb35
brandon <brandoncq036@hotmail.com>
2026-06-13 17:25:11 -0500

    Agregando none

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -74,8 +74,7 @@ export const login = async (req, res) => {
       .cookie("access_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 2 * 60 * 60 * 1000,
+        sameSite: "none",
         path: "/",
       })
       .status(200)
