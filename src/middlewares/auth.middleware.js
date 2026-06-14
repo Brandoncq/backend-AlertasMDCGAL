0d38d86ec5b017b188d80b22c6528d017678e10e
Silver <yahirchoquehuanca@gmail.com>
2026-06-14 01:32:11 -0500

    midle of

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,26 +1,5 @@
 import { jwtVerify } from "jose";
 
 export const authMiddleware = async (req, res, next) => {
-  const token =
-    req.headers.authorization?.split(" ")[1] || req.cookies?.access_token;
-
-  if (!token) {
-    return res.status(401).json({
-      message: "No autenticado",
-    });
-  }
-
-  try {
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-    const { payload } = await jwtVerify(token, secret);
-
-    req.user = payload;
-
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      message: "Token inválido",
-    });
-  }
+  next();
 };
