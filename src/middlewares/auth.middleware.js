2273841e01d05083b290d3f75b22170f15a5f4dc
Silver <yahirchoquehuanca@gmail.com>
2026-06-14 01:38:37 -0500

    miau

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,26 @@
-import { jwtVerify } from "jose";
+import jwt from "jsonwebtoken";
 
-export const authMiddleware = async (req, res, next) => {
-  next();
+export const authMiddleware = (req, res, next) => {
+  let token = req.headers.authorization?.split(" ")[1];
+
+  if (!token || token === "null" || token === "undefined") {
+    token = req.cookies?.access_token;
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      message: "No autenticado",
+    });
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (error) {
+    console.error("JWT Verify Error:", error.message);
+    return res.status(401).json({
+      message: "Token inválido",
+    });
+  }
 };
